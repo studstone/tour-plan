7b6c3c377485a71c6c95888f9eb7711b92f03b33
studstone <nikolas2996@gmail.com>
2022-01-31 19:49:39 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var myswiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+});
