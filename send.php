4ed33de02caf88614acba16b7a404057d4cbbf71
studstone <nikolas2996@gmail.com>
2022-02-20 02:43:24 +0300

    create: add php in newsleter

--- send.php
+++ send.php
@@ -8,6 +8,7 @@ require 'phpmailer/Exception.php';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+$email = $_POST['email'];
 
 // Формирование самого письма
 $title = "Новое обращение Best Tour Plan";
@@ -16,6 +17,7 @@ $body = "
 <b>Имя:</b> $name<br>
 <b>Телефон:</b> $phone<br><br>
 <b>Сообщение:</b><br>$message
+<b>Email:</b><br>$email
 ";
 
 // Настройки PHPMailer
@@ -54,3 +56,4 @@ else {$result = "error";}
 
 // Отображение результата
 header('Location: thankyou.html');
+?>
