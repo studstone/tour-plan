a23396909f2e790aa3717e888541371806eb8cc1
studstone <nikolas2996@gmail.com>
2022-03-01 17:26:16 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -181,4 +181,6 @@ let selector = document.querySelectorAll('input[type="tel"]');
 let im = new Inputmask("+7 (999) 999-99-99");
 im.mask(selector);
 
-AOS.init();
+if ($(window).width() > 991) {
+  AOS.init();
+}
