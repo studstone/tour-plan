70fb93e2feaafdf40818866f0f7f1e0a03809706
studstone <nikolas2996@gmail.com>
2022-03-01 20:27:12 +0300

    fix off AOS in mobile

--- js/main.js
+++ js/main.js
@@ -181,6 +181,9 @@ let selector = document.querySelectorAll('input[type="tel"]');
 let im = new Inputmask("+7 (999) 999-99-99");
 im.mask(selector);
 
-if ($(window).width() > 991) {
-  AOS.init();
-}
+AOS.init({
+  disable: function () {
+    var maxWidth = 768;
+    return window.innerWidth < maxWidth;
+  },
+});
